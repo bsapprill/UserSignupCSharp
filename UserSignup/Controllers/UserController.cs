991bbd4fdadaef9a57ca33af6542568b70dfad91
BenApprill <bsapprill@fullsail.edu>
2018-01-08 01:00:45 -0600

    Continued with User Signup

--- UserSignup/Controllers/UserController.cs
+++ UserSignup/Controllers/UserController.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UserSignup.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace UserSignup.Controllers
 {
     public class UserController : Controller
-    {
-        // GET: /<controller>/
+    {        
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            if(verify == user.Password)
+            {
+                return Redirect("/");
+            }
+            else
+            {               
+                
+                return View();
+            }            
         }
     }
 }
